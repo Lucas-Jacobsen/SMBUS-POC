14f06dc56c5aa4590f907225cfc0dc4354436083
Lucas-Jacobsen <97192501+Lucas-Jacobsen@users.noreply.github.com>
2025-05-05 13:28:11 -0500

    Create X13SAN-L.py

---
+++ X13SAN-L.py
@@ -0,0 +1,21 @@
+import smbus
+import time
+
+I2C_BUS = 1                   # Usually 1 on Raspberry Pi or similar SBC
+DEVICE_ADDR = 0x4D            # PCA9554 I2C address
+REG_OUTPUT = 0x01             # Output port register
+REG_CONFIG = 0x03             # Configuration register
+
+bus = smbus.SMBus(I2C_BUS)
+
+# Set all pins to output (0 = output, 1 = input)
+# For example, set GP0-GP3 to output, GP4-GP7 to input:
+direction_mask = 0b11110000
+bus.write_byte_data(DEVICE_ADDR, REG_CONFIG, direction_mask)
+
+# Set output value for GP0-GP3 (only meaningful for output pins)
+# For example, set GP0 and GP2 high (binary: 00000101)
+output_mask = 0b00000101
+bus.write_byte_data(DEVICE_ADDR, REG_OUTPUT, output_mask)
+
+print("GPIOs configured and output written.")
